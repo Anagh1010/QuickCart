62cba8baa331f229a8f1c23bc3f519b7a6adfd74
Anagh Bajpai <anaghb1010@gmail.com>
2026-04-13 21:04:09 +0530

    made navbar and footer work

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
-        <div className="w-4/5">
+        <div id="about" className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -21,13 +21,13 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Company</h2>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="hover:underline transition" href="#">Home</a>
+                <a className="hover:underline transition" href="/">Home</a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">About us</a>
+                <a className="hover:underline transition" href="/#about">About us</a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">Contact us</a>
+                <a className="hover:underline transition" href="/#contact">Contact us</a>
               </li>
               <li>
                 <a className="hover:underline transition" href="#">Privacy policy</a>
@@ -36,7 +36,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="w-1/2 flex items-start justify-start md:justify-center">
+        <div id="contact" className="w-1/2 flex items-start justify-start md:justify-center">
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
