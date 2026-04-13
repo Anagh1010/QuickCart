c2ef967acd71c811329c43fbd2723bfaae152fbe
Anagh Bajpai <anaghb1010@gmail.com>
2026-04-13 20:52:41 +0530

    fixed my-orders page

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -1,5 +1,7 @@
 import connectDB from "@/config/db";
 import Order from "@/models/Order";
+import Product from "@/models/Product";
+import Address from "@/models/Address";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
